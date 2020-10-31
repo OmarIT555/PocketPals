bfd95a432ec0cc564da4af3ce466fbbda76811e3
Dennis Dalton <ddalton3@ggc.edu>
2020-10-31 17:57:29 -0400

    Made doors consistent and music transfers between scenes

--- Assets/Scripts/music.cs
+++ Assets/Scripts/music.cs
@@ -6,16 +6,17 @@ using UnityEngine.SceneManagement;
 public class music : MonoBehaviour
 {
     
-    // Start is called before the first frame update
-    private void Start(){
-       
-    }
+
     void Awake()
     {
-       
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if (musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
 
     }
 
-    // Update is called once per frame
     
 }
